e03fd1d066ccfc725247807450ac7363c2496bad
KESHAV-SHARMA-2605 <72592100+KESHAV-SHARMA-2605@users.noreply.github.com>
2020-10-09 10:25:37 +0530

    Create Grade_Displayer.cpp

---
+++ Grade_Displayer.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m1, m2, m3, sum, average;
+
+    cout << "Please enter the marks of your three subjects (please enter the marks that you obtained from 100): " << endl;
+    cin >> m1;
+
+    if(m1 <= 0 || m1 >= 100) {
+        cout << "Invalid input!!! You needed to enter the marks of subject that you obtained from 100!!!" << endl;
+        return 0;
+    }
+
+    cin >> m2;
+
+    if(m2 <= 0 || m2 >= 100) {
+        cout << "Invalid input!!! You needed to enter the marks of subject that you obtained from 100!!!" << endl;
+        return 0;
+    }
+
+    cin >> m3;
+
+    if(m3 <= 0 || m3 >= 100) {
+        cout << "Invalid input!!! You needed to enter the marks of subject that you obtained from 100!!!" << endl;
+        return 0;    
+    }
+
+    if(m1 >=0 && m1 <= 100 && m2 >= 0 && m2 <= 100 && m3 >= 0 && m3 <= 100) {
+        sum = m1 + m2 + m3;
+        average = sum / 3;
+    }
+    else {
+        return 0;
+    }
+
+    if(average >= 75) {
+        cout << "Excellent!!! 'A' grade is awarded to you!!! Keep it uo!!!" << endl;
+    }
+    else if(average >= 35 && average < 75) {
+        cout << "Good!!! 'B' grade is awarded to you!!! But, get the 'A' grade next time!!!" << endl;
+    }
+    else {
+        cout << "Poor!!! 'C' grade is awarded to you!!! You need to focus on your studies!!!" << endl;
+    }
+
+    return 0;
+}
