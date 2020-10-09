2cdd213c6fb359405ec8f9fc1e22222b3334f12a
KESHAV-SHARMA-2605 <72592100+KESHAV-SHARMA-2605@users.noreply.github.com>
2020-10-09 12:22:01 +0530

    Create Rectangle_Area-Perimeter_Calculator.cpp

---
+++ Rectangle_Area-Perimeter_Calculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int length, breadth;
+
+int perimeter() {
+    return 2*(length+breadth);
+}
+int area() {
+    return length*breadth;
+}
+
+int main() {
+
+    cout << "Please enter the dimensions of the rectangle(length and breadth) respectively: " << endl;
+    cin >> length >> breadth;
+
+    cout << "The perimeter of the desired rectangle is: " << perimeter() << endl;
+
+    cout << "The area of the desired rectangle is: " << area() << endl;
+
+    return 0;
+}
